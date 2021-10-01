4d5c1b8366a5c33252b3987e5cd6f29d28509e80
sporkupine <cthompson25@murraystate.edu>
2021-09-30 20:09:58 -0500

    updated random color functionality

--- src/app/random-background-color.directive.ts
+++ src/app/random-background-color.directive.ts
@@ -7,19 +7,12 @@ export class RandomBackgroundColorDirective implements OnInit {
   @Input() defaultColor: string = 'transparent';
   @HostBinding('style.backgroundColor') backgroundColor: string;
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
-  randomColor = 'hsla(' + (Math.floor(Math.random()*360) + ', 100%, 70%, 1)')
-  randomColor2 = 'hsla(' + (Math.floor(Math.random()*360) + ', 100%, 70%, 1)')
   ngOnInit() {
-    this.backgroundColor=this.randomColor;
+    this.backgroundColor='hsla(' + (Math.floor(Math.random()*360) + ', 100%, 70%, 1)');
   }
-
-  // @HostListener()
 
   @HostListener('mouseenter') mouseover(eventdata: Event) {
-    this.backgroundColor=this.randomColor2;
-  }
-  @HostListener('mouseleave') mouseleave(eventdata: Event) {
-    this.backgroundColor=this.randomColor;
+    this.backgroundColor='hsla(' + (Math.floor(Math.random()*360) + ', 100%, 70%, 1)');
   }
 
 }
